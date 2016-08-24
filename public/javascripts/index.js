f3b99faeef300dde6fc6f4f1ba1f4801e3762705
Steven Winston <swinston100@hotmail.com>
2016-08-24 18:07:27 +0300

    Loads of progress
    yay!

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,16 +4,23 @@ var socket = io();
 // on every message recived update view
 socket.on('notification', function (data) {
   $('#challenge').html(data.challenge);
+
+  var deadline = moment.tz(data.deadline, "America/New_York");
+
+  console.log(data.deadline);
+
+  $('#clock').countdown(deadline.toDate(), function(event) {
+    $(this).html(event.strftime('%D days %H:%M:%S'))
+    .on("finish.countdown", function(){
+      console.log("hi");
+      socket.emit('expiry', "timesup");
+    })
+  });
+
+
 });
 
 //in an ideal world I want the challenge to change in real time following the deadline (and not just by a page request)
-socket.on('deadline', function (data) {
-  $('#challenge').html(data.challenge);
+socket.on('error', function (data) {
+  $('#error').html(data);
 });
-
-
-// $('#send').click(function () {
-//   console.log("sf");
-//   socket.emit('expiry', "timesup");
-//   return
-// });
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,61 @@
 var fs = require('fs');
 var request = require('request');
-var moment = require('moment');
+//var moment = require('moment');
+var moment = require('moment-timezone');
 var async = require('async');
 var express = require('express');
 var router = express.Router();
 var GoogleSpreadsheet = require('google-spreadsheet');
+
+moment.updateLocale('en', {
+  week: {
+    // Sunday = 0, Monday = 1 etc
+    doy : 7, // This sets the first Monday of the year to be the start of week 2! Trust me it just does!
+    dow : 1  // Where Monday is the first day of the week.
+  }
+});
 
 
 module.exports = function(io) {
 
   router.get('/', function(req, res){
 
-    var weeknumber =  moment().isoWeek();
+    res.render("index")
+
+    var time = moment().tz("America/New_York")
+
+    // //for testing
+    // var formattedtime = time.format("HH:mm")
+    // console.log(formattedtime);
+    // var localeData = moment.localeData()
+    // console.log("first date of week: " + localeData.firstDayOfWeek());
+    // console.log("first day of year: " + localeData.firstDayOfYear());
+
+    var weeknumber = time.week()
+
+    // //for testing
+    // var date = moment("2017-01-01")
+    // weeknumber = date.week()
+    // console.log(weeknumber);
+
+    var doc = new GoogleSpreadsheet('12oNY21VWHpS4bxYDv86AEjWPifeIJrVhRaK4_wuu7DY');
+    var sheet;
+
+    readSheet(weeknumber, function(err,data){
+      if(err){
+        io.emit('error', "Something went wrong: " + err.message)
+      }
+      io.emit('notification', data);
+    });
+
+  })
+
+
+  function readSheet(weeknumber, cb) {
 
     var doc = new GoogleSpreadsheet('12oNY21VWHpS4bxYDv86AEjWPifeIJrVhRaK4_wuu7DY');
     var sheet;
     var data = {};
-    var deadline;
-
-    console.log(moment.utc().format("DD-MM-YYYY HH mm"));
 
     async.series([
       function setAuth(step) {
@@ -28,9 +65,14 @@ module.exports = function(io) {
       },
       function getInfoAndWorksheets(step) {
         doc.getInfo(function(err, info) {
-          console.log('Loaded doc: '+info.title+' by '+info.author.email);
-          sheet = info.worksheets[0];
-          step();
+          if(err){
+            return cb(err, null)
+          }
+          else{
+            console.log('Loaded doc: '+info.title+' by '+info.author.email);
+            sheet = info.worksheets[0];
+            step();
+          }
         });
       },
       function getData(step) {
@@ -38,35 +80,43 @@ module.exports = function(io) {
         sheet.getRows({
           offset: 1,
         }, function( err, rows ){
+
+          if(err){
+            return cb(err,null)
+          }
+
           console.log('Read '+rows.length+' rows');
 
-          data.challenge = rows[weeknumber-1].challenge;
-          data.credit = rows[weeknumber-1].extracredit;
-          deadline = rows[weeknumber-1].deadline;
-          io.emit('notification', data);
+          if(rows[weeknumber-1]){
+
+            data.challenge = rows[weeknumber-1].challenge;
+            data.credit = rows[weeknumber-1].extracredit;
+            data.deadline = rows[weeknumber-1].deadline;
+
+            return cb(null,data)
+          }
+          else {
+            return cb(new Error("No data found for this week"), null)
+          }
 
           //need to create a timer that fires this when it expires.
-          io.emit('deadline', deadline);
-
           step();
         });
       }
-
     ]);
-    res.render("index")
-  })
+  }
 
 
-  // io.on("connection", function(socket)
-  // {
-  //   //on page load
-  //   //for realtime update, broadcast to all clients
-  //   socket.on('expiry', function(data){
-  //     socket.emit('notification', {challenge: "hello"});
-  //     socket.broadcast.emit('notification', {challenge: "hello"});
-  //   })
-  //
-  // });
+  io.on("connection", function(socket)
+  {
+    //on page load
+    //for realtime update, broadcast to all clients
+    socket.on('expiry', function(data){
+      socket.emit('notification', {challenge: "hello"});
+      socket.broadcast.emit('notification', {challenge: "hello"});
+    })
+
+  });
 
 
   return router;
